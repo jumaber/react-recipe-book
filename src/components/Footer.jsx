136e1aaeea6638f889f536b8306c369b8b1d810a
Julia Mari Bernaus <jumaber@gmail.com>
2025-03-24 11:25:14 +0100

    updated Github Repo Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer (){
    return (
     <div className="flex flex-row justify-center content-center items-center">
-      <a href="https://github.com/jumaber/React_Recipe_Book_App" target="_blank" className="footer-link">Link to our Git Repo</a>
+      <a href="https://github.com/jumaber/react-recipe-book" target="_blank" className="footer-link">Link to our Git Repo</a>
     </div>
     )
   }
