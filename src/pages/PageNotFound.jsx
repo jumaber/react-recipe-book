345601eca90dfbfe02f7d44410e8888bb2c4110e
Julia Mari Bernaus <jumaber@gmail.com>
2025-03-24 11:52:40 +0100

    styled 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,22 @@
-export function PageNotFound(){
+import { Link } from "react-router-dom";
+import broccoliImg from "../assets/broccoli.svg";
 
-  return(
-    <>
-      Page Not Found
-    </>
-  )
+export function PageNotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen ml-64 p-4 bg-orange-100 text-center">
+      <h1 className="text-5xl font-black mb-4 text-teal-950">404: Broccoli not found.</h1>
+      <p className="text-xl mb-10 text-teal-800">Don’t worry, most people avoid it anyway.</p>
+
+      {/* Sneaky Broccoli */}
+      <img
+        src={broccoliImg}
+        alt="Broccoli sneaking away"
+        className="w-64 animate-sneak opacity-80"
+      />
+
+      <Link to="/" className="btn-sm mt-12">
+        Go back to tastier things
+      </Link>
+    </div>
+  );
 }
